c7eac5d48dfa91b8e20262929d84c180e32a14a1
Ocean_Moist <Rohanzealous@Gmail.com>
2023-03-26 19:13:38 -0700

    fix

--- src/main/java/com/arc852/Main.java
+++ src/main/java/com/arc852/Main.java
@@ -7,34 +7,46 @@ import edu.wpi.first.util.CombinedRuntimeLoader;
 import edu.wpi.first.util.WPIUtilJNI;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Main {
-    private DoublePublisher dpl;
+    private static DoublePublisher dpl;
     private static final int HUB_PORT = 0;
     public static final int ENC_RESOLUTION = 96;
     public static final double ENC_TO_RADS = 1.0 / ENC_RESOLUTION * 2 * Math.PI;
-    private static Encoder enc;
 
-    public static void main(String[] args) throws IOException, PhidgetException {
-        NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
-        WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
-        WPIMathJNI.Helper.setExtractOnStaticLoad(false);
-        CameraServerJNI.Helper.setExtractOnStaticLoad(false);
+    public static void main(String[] args) throws IOException, PhidgetException, InterruptedException {
+      NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+      WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
+      WPIMathJNI.Helper.setExtractOnStaticLoad(false);
+      CameraServerJNI.Helper.setExtractOnStaticLoad(false);
 
-        enc = new Encoder();
-        enc.setHubPort(HUB_PORT);
+      Encoder enc = new Encoder();
+      enc.setHubPort(HUB_PORT);
 
-        CombinedRuntimeLoader.loadLibraries(Main.class, "wpiutiljni", "wpimathjni", "ntcorejni",
-                "cscorejnicvstatic");
-        new Main().run();    }
+      CombinedRuntimeLoader.loadLibraries(Main.class, "wpiutiljni", "wpimathjni", "ntcorejni",
+          "cscorejnicvstatic");
 
-    private void run() {
-        NetworkTableInstance inst = NetworkTableInstance.getDefault();
-        var topic = inst.getDoubleTopic("phidget/dial");
-        topic.setPersistent(true);
-        dpl = topic.publish();
-        dpl.setDefault(0.0);
 
-        enc.addPositionChangeListener(posEvent -> dpl.set(posEvent.getPositionChange()*ENC_TO_RADS));
+      NetworkTableInstance inst = NetworkTableInstance.getDefault();
+      inst.startClient4("phidget with ur balls");
+      inst.setServer("localhost");
+      inst.startDSClient();
+      var topic = inst.getDoubleTopic("phidget/dial");
+      topic.setPersistent(true);
+      dpl = topic.publish();
+      dpl.setDefault(0.0);
+
+      AtomicReference<Double> pos = new AtomicReference<>((double) 0);
+
+      enc.addPositionChangeListener(posEvent -> {
+        pos.updateAndGet(v -> v + posEvent.getPositionChange() * ENC_TO_RADS);
+        dpl.set(pos.get());
+      });
+
+      enc.open();
+
+      Thread.currentThread().join();
     }
+
 }
